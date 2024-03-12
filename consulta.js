775e39d37eda6835261f7f7392ba82ed4be2974d
SebastianGit110 <124834599+SebastianGit110@users.noreply.github.com>
2024-03-12 14:51:40 -0500

    Se añade metodo GET para traer usuario por id

--- consulta.js
+++ consulta.js
@@ -1,37 +1,49 @@
-const mysql = require("mysql")
-const express = require("express")
-const app = express()
+const mysql = require("mysql");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
 
 const conexion = mysql.createConnection({
-    host : "localhost",
-    user : "root",
-    password : "RootUmb",
-    database : "gestion"
+  host: "localhost",
+  user: "root",
+  password: "Rootumb",
+  database: "gestion",
 });
 
-conexion.connect((error)=>{
-    if (error) throw error 
-    console.log('conexion exitosa')
+conexion.connect((error) => {
+  if (error) throw error;
+  console.log("conexion exitosa");
 });
 
-app.get('/', function(req, resp){
-    conexion.query('SELECT * FROM users', function(error, rows){
-        if(!!error){
-            console.log('Conexion fallida')
-        }
-        else{
-            console.log('Consulta de la info de la tabla')
-            console.log(rows)
-            resp.json(rows)           
-            conexion.end();
-        }
-    });
+// Permite conseguir info de la BD
+app.get("/api/personas", function (req, resp) {
+  conexion.query("SELECT * FROM users", function (error, rows) {
+    if (!!error) {
+      console.log("Conexion fallida");
+    } else {
+      console.log("Consulta de la info de la tabla");
+      console.log(rows);
+      resp.json(rows);
+      // conexion.end() // Cierra la conexion y no me permite traer mas datos, pero si la comento la conexion
+      // Queda abierta y se pueden seguir haciendo consultas
+    }
+  });
 });
 
-app.use(express.static('public')); // Tienen que correrse despues de la consulta porque si se hace antes, el html va a solicitar el mismo html
-// Hace los arcivos estaticos y se pueden acceder http://localhost:1337/index.html
-
-const PORT = 1337;
-app.listen(PORT, () => {
-    console.log(`Servidor escuchando en el puerto ${PORT}`);
+app.get("/api/personas/:id", (req, res) => {
+  conexion.query(
+    "SELECT * FROM users WHERE id = ?",
+    [req.params.id],
+    (error, rows) => {
+      if (error) {
+        throw error;
+      } else {
+        res.send(rows);
+      }
+    }
+  );
 });
+
+app.listen(1338);
